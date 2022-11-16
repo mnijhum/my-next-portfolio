8b6ab7508e9dc24695cb0391233b46ebb2dbba15
Mushfikunnabi Nijhum <m.nijhum0@gmail.com>
2022-11-17 03:05:35 +0600

    ssh github action added

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-$ ssh -i ${{ secrets.SSH_PRIVATE_KEY }} ${{secrets.SSH_IP}}
+$ ssh -o StrictHostKeyChecking=no ${{ secrets.SSH_PRIVATE_KEY }} ${{secrets.SSH_IP}}
 $ docker-compose pull
 $ docker-compose up -d
