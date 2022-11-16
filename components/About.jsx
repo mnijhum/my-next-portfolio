c4503db65c6f2cf40834bd5b3bb55173ba6d552a
Mushfikunnabi Nijhum <m.nijhum0@gmail.com>
2022-11-17 02:52:24 +0600

    about me changed a little

--- components/About.jsx
+++ components/About.jsx
@@ -20,13 +20,13 @@ const About = () => {
         </h1>
 
         <p className={`${design.paragraph} ${design.paddingY} text-start`}>
-          I am a dreamer who dreams about being a successful person in every
-          aspect of my life. Currently, I have been working as a full time
-          software engineer at Intercloud Limited which is located in Dhaka,
-          Bangladesh. Besides, during my leisure time, you can find me playing
-          FIFA, watching TV-Series, Movies or hanging out with my friends.
-          Whenever I get a chance to do something, I usually do not stop until I
-          finish it.
+          I am a professional Software Engineer who dreams about being a
+          successful person in every aspect of my life. Currently, I have been
+          working as a full time software engineer at Intercloud Limited which
+          is located in Dhaka, Bangladesh. Besides, during my leisure time, you
+          can find me playing FIFA, watching TV-Series, Movies or hanging out
+          with my friends. Whenever I get a chance to do something, I usually do
+          not stop until I finish it.
         </p>
       </div>
     </section>
