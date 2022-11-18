7caa3970f64e29e7a5d7dd92c1ef41b25ed89b42
Mushfikunnabi Nijhum <m.nijhum0@gmail.com>
2022-11-18 20:07:36 +0600

    test push

--- components/About.jsx
+++ components/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
 
         <p className={`${design.paragraph} ${design.paddingY} text-start`}>
           I am a professional Software Engineer who dreams about being a
-          successful person in every aspect of my life. Currently, I have been
+          successful person in every aspect of life. Currently, I have been
           working as a full time software engineer at Intercloud Limited which
           is located in Dhaka, Bangladesh. Besides, during my leisure time, you
           can find me playing FIFA, watching TV-Series, Movies or hanging out
